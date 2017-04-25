8a7a6fd9cc042dfea503b6342fec779e993130e1
Rafael Frank <rafael.frank@sap.com>
2017-04-24 23:19:52 -0300

    Adjusting UI and Services

--- comparison/app1/src/collection/Message.js
+++ comparison/app1/src/collection/Message.js
@@ -7,7 +7,10 @@ module.exports = (()=>{
   let Schema = new Mongoose.Schema({
     user: String,
     message: String,
-    date: Date
+    date: {
+      type: Date,
+      default: Date.now 
+    }
   });
 
   return Mongoose.model('Message', Schema);
