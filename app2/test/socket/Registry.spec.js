599f4a29b4b203b4627ea3069b8c5d488617e628
Rafael Frank <rafael.frank@sap.com>
2017-05-04 13:07:03 -0300

    Fixing registry ut

--- app2/test/socket/Registry.spec.js
+++ app2/test/socket/Registry.spec.js
@@ -1,8 +1,5 @@
-return
 require('../bootstrap.js');
 
-const FILE_PATH = '../../src/socket/Registry.js';
-let Registry;
 let fnFakeSocketIO;
 const oFakeServer = {};
 let oFakeMessageHandler;
@@ -10,59 +7,64 @@ const oFakeSocket = {};
 
 describe('Registry - Tests', () => {
 
-  beforeEach(() => {
+  before(() => {
     //Mock dependencies
     oFakeSocketIO = {
       on: sinon.stub()
     }
-    fnFakeSocketIO = sinon.stub().returns(oFakeSocketIO);
-    mock('socket.io', fnFakeSocketIO);
 
     oFakeMessageHandler = {
       register: sinon.spy()
     }
-    mock('../../src/socket/MessageHandler.js', oFakeMessageHandler);
 
-    Registry = mock.reRequire(FILE_PATH);
-  });
-
-  after(() => {
-    mock.stop('socket.io');
-    mock.stop('../../src/socket/MessageHandler.js');
+    injectjs.core.Import.setModule('tcc.src.external.socketio', oFakeSocketIO);
+    injectjs.core.Import.setModule('tcc.src.socket.MessageHandler', oFakeMessageHandler);
   });
 
   describe('Inspection', () => {
-    it('Should initialize the Socket IO dependency with the Server instance', () => {
-      Registry(oFakeServer);
 
-      chai.expect(fnFakeSocketIO.callCount).to.equal(1);
-      chai.expect(fnFakeSocketIO.args[0][0]).to.equal(oFakeServer);
+    it('Should attach the registerHandlers API on the "connection" callback', (done) => {
+      define([
+        'original.tcc.src.socket.Registry'
+      ], Registry => {
+        chai.expect(oFakeSocketIO.on.callCount).to.equal(1);
+        chai.expect(oFakeSocketIO.on.args[0][0]).to.equal("connection");
+        chai.expect(oFakeSocketIO.on.args[0][1]).to.equal(Registry.registerHandlers);
+        done();
+      });
     });
-    it('Should attach the registerHandlers API on the "connection" callback', () => {
-      let oInstance = Registry(oFakeServer);
-
-      chai.expect(oFakeSocketIO.on.callCount).to.equal(1);
-      chai.expect(oFakeSocketIO.on.args[0][0]).to.equal("connection");
-      chai.expect(oFakeSocketIO.on.args[0][1]).to.equal(oInstance.registerHandlers);
+    it('Should be an object', (done) => {
+      define([
+        'original.tcc.src.socket.Registry'
+      ], Registry => {
+        chai.expect(typeof Registry).to.equal("object");
+        done();
+      });
     });
-    it('Should be a function', () => {
-      chai.expect(typeof Registry).to.equal("function");
-    });
-    it('Should return a Registry instance if executed', () => {
-      chai.expect(typeof Registry(oFakeServer)).to.equal("object");
+    it('Should be a singleton', (done) => {
+      define([
+        'original.tcc.src.socket.Registry',
+        'original.tcc.src.socket.Registry'
+      ], (Registry1, Registry2) => {
+        chai.expect(Registry1 === Registry2).to.equal(true);
+        done();
+      });
     });
   });
 
   describe('API', () => {
     describe('registerHandlers', () => {
-      it('Should register all Socket All Handlers', () => {
-        Registry(oFakeServer);
+      it('Should register all Socket All Handlers', (done) => {
+        define([
+          'original.tcc.src.socket.Registry',
+          'original.tcc.src.socket.Registry'
+        ], (Registry1, Registry2) => {
+          oFakeSocketIO.on.args[0][1](oFakeSocket);
 
-        //Fake 'connection' callback call
-        oFakeSocketIO.on.args[0][1](oFakeSocket);
-
-        chai.expect(oFakeMessageHandler.register.callCount).to.equal(1);
-        chai.expect(oFakeMessageHandler.register.args[0][0]).to.equal(oFakeSocket);
+          chai.expect(oFakeMessageHandler.register.callCount).to.equal(1);
+          chai.expect(oFakeMessageHandler.register.args[0][0]).to.equal(oFakeSocket);
+          done();
+        });
       });
     });
   });
